b3824bd37c954580f265c98cf139209a423a784c
ty85sr <81002651+ty85sr@users.noreply.github.com>
2022-10-10 06:35:43 -0500

    Create lawnchair.mk

---
+++ lawnchair.mk
@@ -0,0 +1,14 @@
+# Lawnchair
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lawnchair
+
+PRODUCT_COPY_FILES += \
+    vendor/lawnchair/libs/libnrb.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libnrb.so
+
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnchairFrameworksOverlay \
+    LawnchairOverlay \
+    Lawnicons \
+    lawnchair-hiddenapi-package-whitelist \
+    privapp-permissions-lawnchair
